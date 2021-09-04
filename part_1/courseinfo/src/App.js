0a97c1e64328b69e7050f118b2c7cb92568b79c5
KianYang-Lee <ken.lee.kianyang@gmail.com>
2021-09-04 22:31:15 +0800

    Fix solutions

--- part_1/courseinfo/src/App.js
+++ part_1/courseinfo/src/App.js
@@ -23,21 +23,14 @@ const Part = (props) => {
   );
 };
 
-const Content = () => {
+const Content = (props) => {
   // Refactor the Content component so that it does not render any names of parts or their number of exercises by itself.
   //  Instead it only renders three Part components of which each renders the name and number of exercises of one part.
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
-
   return (
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   );
 };
@@ -63,7 +56,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content />
+      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   );
