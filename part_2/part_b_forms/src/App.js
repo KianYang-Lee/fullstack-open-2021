ed3ed68958d7e7ff766c9155500c8f9ce9ecee7b
KianYang-Lee <ken.lee.kianyang@gmail.com>
2021-09-08 14:25:23 +0800

    Exercise for part 2b

---
+++ part_2/part_b_forms/src/App.js
@@ -0,0 +1,98 @@
+// ------------------------------ FORMS -------------------------------------- //
+// // source code from https://github.com/FullStack-HY/part2-notes/blob/part2-1/src/App.js
+// import React, { useState } from 'react';
+// import Note from './components/Note';
+
+
+// const App = (props) => {
+//   const [notes, setNotes] = useState(props.notes);
+//   const [newNote, setNewNote] = useState('a new note...');
+
+//   const addNote = (event) => {
+//     event.preventDefault();
+//     const noteObject = {
+//       content: newNote,
+//       date: new Date().toISOString(),
+//       important: Math.random() < 0.5,
+//       id: notes.length + 1,
+//     };
+//     setNotes(notes.concat(noteObject));
+//     setNewNote('');
+//   };
+
+//   const handleNoteChange = (event) => {
+//     console.log(event.target.value);
+//     setNewNote(event.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <h1>Notes</h1>
+//       <ul>
+//         {notes.map(note =>
+//           <Note key={note.id} note={note} />
+//         )}
+//       </ul>
+//       <form onSubmit={addNote}>
+//         <input value={newNote} onChange={handleNoteChange} />
+//         <button type='submit'>save</button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// ------------------------- FILTERING DISPLAYED ELEMENTS ---------------------- //
+import React, { useState } from 'react';
+import Note from './components/Note';
+
+
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('a new note...');
+  const [showAll, setShowAll] = useState(true);
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true);
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+    setNotes(notes.concat(noteObject));
+    setNewNote('');
+  };
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type='submit'>save</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
