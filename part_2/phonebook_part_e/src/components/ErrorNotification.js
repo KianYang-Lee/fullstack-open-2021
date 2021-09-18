f3e1a21568fc2610819325b0d5882cd3800f0937
KianYang-Lee <ken.lee.kianyang@gmail.com>
2021-09-18 16:14:30 +0800

    Solutions for phonebook part 2e

---
+++ part_2/phonebook_part_e/src/components/ErrorNotification.js
@@ -0,0 +1,21 @@
+const ErrorNotification = ({ errorMessage }) => {
+  const notificationStyle = {
+    color: "red",
+    background: "lightgrey",
+    fontSize: 20,
+    borderStyle: "solid",
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  };
+  if (errorMessage === null) {
+    return null;
+  }
+  return (
+    <div style={notificationStyle}>
+      {errorMessage}
+    </div>
+  );
+};
+
+export default ErrorNotification;
