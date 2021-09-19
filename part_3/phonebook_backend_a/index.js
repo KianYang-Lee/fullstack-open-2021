425c21f2dc9240b5cff3b9dc5a1c6118163fe070
KianYang-Lee <ken.lee.kianyang@gmail.com>
2021-09-19 17:26:50 +0800

    Include solutions for phonebook backend part a

---
+++ part_3/phonebook_backend_a/index.js
@@ -0,0 +1,99 @@
+// ============== 3.1-6 PHONEBOOK BACKEND STEP 1 TO 6 ======================= //
+let persons = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  },
+  {
+    "id": 5,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+];
+
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+// STEP 3.2 
+app.get("/info", (request, response) => {
+  const date = new Date();
+  const total = persons.length;
+  return response.send(
+    `
+    <p>Phonebook has info for ${total} people</p>
+    <p>${date}</p>
+    `
+  );
+});
+
+// STEP 3.3
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+// STEP 3.4
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(person => person.id !== id);
+  response.status(204).end();
+});
+
+// STEP 3.5 and 3.6
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  if (!(body.name && body.number)) {
+    return response.status(400).json({
+      error: 'Name or number is missing'
+    });
+  }
+
+  matched_name = persons.find(person => person.name === body.name);
+  if (matched_name) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    // Reverse min-max normalization
+    id: Math.floor(Math.random() * (999 - 1) + 1)
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const PORT = 3002;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
