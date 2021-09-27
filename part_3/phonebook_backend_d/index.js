9614acd30f1ed7c05e33d4a1a601cfd0f672a911
KianYang-Lee <ken.lee.kianyang@gmail.com>
2021-09-27 19:36:41 +0800

    Added eslint for part 3d

--- part_3/phonebook_backend_d/index.js
+++ part_3/phonebook_backend_d/index.js
@@ -2,6 +2,7 @@
 
 require('dotenv').config();
 
+const process = require('process');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -9,36 +10,37 @@ const app = express();
 const Person = require('./models/person');
 app.use(express.json());
 app.use(express.static('build'));
+// eslint-disable-next-line no-unused-vars
 morgan.token('body', (request, response) => {
   return JSON.stringify(request.body);
 });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors());
 
-app.get("/api/persons", (request, response) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons);
   });
 });
 
-app.get("/info", (request, response) => {
-  const date = new Date();
-  const total = persons.length;
-  return response.send(
-    `
-    <p>Phonebook has info for ${total} people</p>
+app.get('/info', (request, response) => {
+  Person.countDocuments({}).then(count => {
+    const date = new Date();
+    response.send(`
+    <p>Phonebook has info for ${count} people</p>
     <p>${date}</p>
-    `
-  );
+    `);
+  });
+
 });
 
-app.get("/api/persons/:id", (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person =>
     response.json(person))
     .catch(error => next(error));
 });
 
-app.post("/api/persons", (request, response, next) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body;
   if (!(body.name && body.number)) {
     return response.status(400).json({
@@ -58,6 +60,7 @@ app.post("/api/persons", (request, response, next) => {
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
+    // eslint-disable-next-line no-unused-vars
     .then(result => {
       response.status(204).end();
     })
--- part_3/phonebook_backend_d/models/person.js
+++ part_3/phonebook_backend_d/models/person.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
+const process = require('process');
 
 const url = process.env.MONGODB_URI;
 
 console.log('connecting to', url);
 
 mongoose.connect(url)
+  // eslint-disable-next-line no-unused-vars
   .then(result => {
     console.log('connected to MongoDB');
   })
   .catch((error) => {
-    console.log('error connected to MongoDB', error.message);
+    console.log('error connected to MongoDB', error);
   });
 
 const personSchema = new mongoose.Schema({
